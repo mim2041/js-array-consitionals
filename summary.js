048b09becd5369cdf76a6911544d0f91ddbe444e
mim2041 <suraiyamim338@gmail.com>
2023-01-26 22:58:53 +0600

    practice porblem

---
+++ summary.js
@@ -0,0 +1,18 @@
+var destinations = ['sajek', 'coxsbazar', 'nepal', 'bali', 'sp'];
+
+console.log(destinations[2]);
+destinations[2]='thailand';
+var index = destinations.indexOf('bali');
+
+destinations.push('bhutan');
+destinations.pop();
+
+if(destinations[1]==='sajek'){
+
+}
+else if(destinations.length !== 3){
+
+}
+else{
+    
+}
