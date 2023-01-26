7a25ae1f08e2f27543d3c72ff2a399d654e45994
mim2041 <suraiyamim338@gmail.com>
2023-01-26 22:33:31 +0600

    if else conditions more examples

---
+++ multi-stage-conditions.js
@@ -0,0 +1,13 @@
+var money = 50;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+if(danishPrice < money){
+    console.log("Dan Dan Danish Khamu");
+}
+else if(butterBread < money){
+    console.log("Butter bon khamu");
+}
+else{
+    console.log("khali ca e sasther jonno valo");
+}
