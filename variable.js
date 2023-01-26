79071c8b51a2fdb57fe4323e68ee5ed5485c33bf
mim2041 <suraiyamim338@gmail.com>
2023-01-26 20:19:47 +0600

    variable recap

---
+++ variable.js
@@ -0,0 +1,10 @@
+// Number type variables
+var sunglassPrice = 120;
+
+// string type variable
+var specialOneName = "Ariana Grande";
+
+// boolean type variable
+var isSingle = true;
+
+// two types of number: integer 120
