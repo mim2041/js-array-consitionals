d79834605bf804d3e30f9bdcec6ff6169b15c7d0
mim2041 <suraiyamim338@gmail.com>
2023-01-26 20:55:01 +0600

    array push pop operation

---
+++ push-pop.js
@@ -0,0 +1,13 @@
+var numbers = [78, 45, 98, 45];
+numbers.push(63);
+console.log(numbers);
+
+var friends = ['balam', 'kalam', 'salam'];
+friends.push('gelam');
+friends.push('pailam');
+// console.log(friends);
+
+// use pop to get the last number
+numbers.pop();
+numbers.pop();
+console.log(numbers);
