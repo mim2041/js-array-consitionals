0ac31ea760045604ddd3fd76e3bbabb30d8292de
mim2041 <suraiyamim338@gmail.com>
2023-01-26 22:04:08 +0600

    if else statement conditionals

---
+++ conditional.js
@@ -0,0 +1,25 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+// if price is less than my budget, i will buy the iphone
+// if iphone price is less than my budget
+// i will buy the iphone
+
+/*
+if (condition){
+    will execute if the condition is true
+}
+
+*/
+
+if(iphonePrice < myBudget){
+    console.log('i phone kine futani marbo');
+}
+
+var chickenPrice = 500;
+var myMoney = 500;
+if(chickenPrice <= myMoney){
+    console.log('Murgir raan khamu and haddi chibamu!!!');
+}
+else{
+    console.log('Smashed potato and lentils soup');
+}
