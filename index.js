915ba943a3e2275e1451493b0e4a547139268bed
mim2041 <suraiyamim338@gmail.com>
2023-01-26 20:44:44 +0600

    array index

---
+++ index.js
@@ -0,0 +1,16 @@
+var numbers = [45, 68, 78, 56, 89, 98];
+// get elements by index
+// console.log(numbers[0]);
+
+var second = numbers[1];
+// console.log(second);
+
+numbers[1] = 77;
+// console.log(numbers);
+
+var element = numbers[3];
+// console.log(element);
+
+// var positionIndex = numbers.indexOf(98);
+var positionIndex = numbers.indexOf(99);
+console.log(positionIndex);
