ff1f62f2ae06316bc8821c1eae9652a6b7366b60
mim2041 <suraiyamim338@gmail.com>
2023-01-26 20:27:17 +0600

    array

---
+++ array.js
@@ -0,0 +1,7 @@
+var friendsAge = [11, 21, 32, 14, 15, 34];
+var picnicFee = [5000, 2000, 4000, 150];
+var nayikas = ['mahi', 'opu', "sabnoor", 'sabana'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+// console.log(vowels);
+console.log(vowels.length);
